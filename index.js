5bdee9008754fab377cf20787c5603f3d68e9820
Ashray Parmar <ashrayparmar0307@gmail.com>
2021-01-07 20:42:59 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+setInterval(() => {
+
+    const date = new Date();
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+    const second = date.getSeconds();
+
+    
+
+
+}, 1000);
