51e9b0b794c3d7b84f2a9ee8da4af031fc2e9b4e
Ashray Parmar <ashrayparmar0307@gmail.com>
2021-01-07 23:19:46 +0530

    DONE

--- index.js
+++ index.js
@@ -5,7 +5,18 @@ setInterval(() => {
     const minute = date.getMinutes();
     const second = date.getSeconds();
 
-    
+  const secondHand = document.querySelector('#second');
+  const minsHand = document.querySelector('#minute');
+  const hourHand = document.querySelector('#hour');
 
+  const secondsDegrees = ((second / 60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`
+
+  const minutesDegrees = ((minute / 60) * 360) + ((second/60)*6) + 90;
+  minsHand.style.transform = `rotate(${minutesDegrees}deg)`
+
+  const hourDegrees = ((hour / 12) * 360) + ((minute/60)*30) + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`
+ 
 
 }, 1000);
